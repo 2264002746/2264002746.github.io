d40901857ed43da06467603290c04ec64c4186be
猩红 <67999826+2264002746@users.noreply.github.com>
2022-09-30 09:20:58 +0800

    Update 原神.sh

--- 原神/原神.sh
+++ 原神/原神.sh
@@ -1,16 +1,5 @@
 
- pkg install wget -y
- termux-setup-storage
- ​wget https://2264002746.github.io/原神/gm.zip.001
- ​wget https://2264002746.github.io/原神/gm.zip.002
- wget https://2264002746.github.io/原神/gm.zip.003
- wget https://2264002746.github.io/原神/gm.zip.004
- wget https://22640 
-
- #!/bin/bash 
-
- #simple script menu 
-
+ 
  function arm { 
 
  clear 
@@ -23,13 +12,10 @@
 
  echo "下载资源文件....."
 
- termux-setup-storage
-
- wget  https://2264002746.github.io/原神/gm.zip.001
-
+ 
  
 
- wget  https://2264002746.github.io/原神/gm.zip.002
+ wget  https://2264002746.github.io/神/gm.zip.002
 
  
 
@@ -42,7 +28,6 @@
  
 
  wget  https://2264002746.github.io/原神/gm.zip.005
-
  
 
  wget  https://2264002746.github.io/原神/gm.zip.006
@@ -51,136 +36,129 @@
 
  wget  https://2264002746.github.io/原神/gm.zip.007
 
- 
+
 
  wget  https://2264002746.github.io/原神/gm.zip.008
-
  
 
  wget  https://2264002746.github.io/原神/gm.zip.009
 
- 
 
  wget  https://2264002746.github.io/原神/gm.zip.010
 
  
-
  wget  https://2264002746.github.io/原神/gm.zip.011
 
  
 
  wget  https://2264002746.github.io/原神/gm.zip.012
 
- 
 
  wget  https://2264002746.github.io/原神/gm.zip.013
-
  echo "安装ZIP....." 
 
  pkg install zip -y 
 
- echo "解压文件"
+ echo "解压文
 
- termux-setup-storage
+ termux-setup-storag
 
- cat ./gm.zip.* > gm.zip
+ cat ./gm.zip.* > gm.zi
 
  unzip ./gm.zip
 
  rm -rf ./gm.zip.*
 
- echo "安装完成" 
-
+ echo "安装完成"
   
-
   
 
  }  
  ​#!​/bin/bash 
  ​#​simple script menu 
- ​function​ ​arm​ { 
+ ​function​ ​arm​ {
  ​clear 
  ​echo​ ​"​安装中wget.....​"
- pkg install wget -y
+ pkg install wget -
  
  ​echo​ ​"​下载资源文件.....​"
  termux-setup-storage
  ​wget  https://2264002746.github.io/原神/gm.zip.001
   
- ​#!​/bin/bash 
+ ​#!​/bin/bash
  ​#​simple script menu 
  ​function​ ​arm​ { 
- ​clear 
+ ​clear
  ​echo​ ​"​安装中wget.....​"
  pkg install wget -y
  
  ​echo​ ​"​下载资源文件.....​"
  termux-setup-storage
- ​wget https://2264002746.github.io/原神/gm.zip.001
+ ​wget https://2264002746.github.io/原神/gm.zip.00
  
  ​wget https://2264002746.github.io/原神/gm.zip.002
- 
+
  wget https://2264002746.github.io/原神/gm.zip.003
  
  wget https://2264002746.github.io/原神/gm.zip.004
  
  wget https://2264002746.github.io/原神/gm.zip.005
- 
+
  wget https://2264002746.github.io/原神/gm.zip.006
  
- wget https://2264002746.github.io/原神/gm.zip.007
+ wget https://2264002746.github.io/原神/gm.zip.00
  
  wget https://2264002746.github.io/原神/gm.zip.008
  
  wget https://2264002746.github.io/原神/gm.zip.009
  
- wget https://2264002746.github.io/原神/gm.zip.010
+ wget https://2264002746.github.io/原神/gm.zip.01
  
  wget https://2264002746.github.io/原神/gm.zip.011
- 
+
  wget https://2264002746.github.io/原神/gm.zip.012
  
  wget https://2264002746.github.io/原神/gm.zip.013
  ​echo​ ​"​安装ZIP.....​" 
  ​pkg install zip -y 
- echo "解压文件"
+ echo "解压文件
  termux-setup-storage
  cat ./gm.zip.* > gm.zip
- unzip ./gm.zip
+ unzip ./gm.zi
  rm -rf ./gm.zip.*
  ​echo​ ​"​安装完成​" 
   
   
  ​} 
  ​function​ ​mysql​ { 
- ​clear 
+ ​clear
   echo​ ​"​运行jar​" 
-  java -jar gm.jar
+  java -jar gm.ja
  ​} 
  ​function​ ​Magisk​ { 
  ​clear 
  ​echo​ ​"​等待安装.....​" 
   
  ​} 
- ​function​ ​menu​ { 
+ ​function​ ​menu​ {
  ​clear  
- ​echo 
+ ​echo
  ​echo​ -e ​"​\t\t\t 应用主菜单​" 
  ​echo​ -e ​"​\t1.原神服务器资源(安卓)​" 
  ​echo​ -e ​"​\t2.启动服务器端(安卓)​"​  
  ​echo​ -e ​"​\t3. 面具安装​"​  
  ​echo​ -e ​"​\t0. 退出脚本\n\n​" 
  ​echo​ -en ​"​\t\t输入安装序列号:​"​  
- ​read​ -n 1 option 
+ ​read​ -n 1 option
  ​} 
-  
+ 
  ​while​ [ 1 ] 
  ​do​  
  ​    menu 
  ​    ​case​ ​$option​ ​in 
  ​    0)break ;; 
  ​    1)arm ;; 
- ​    2)mysql ;; 
+ ​    2)mysql ;;
  ​    3)Magisk ;; 
  ​    ​*​) 
  ​        clear 
@@ -189,43 +167,9 @@
  ​    ​echo​ -en ​"​\n\n\t\t 按任意键返回​" 
  ​    ​read​ -n 1 line 
  ​done 
+ ​clea
+ ​
  ​clear
- ​wget  https://2264002746.github.io/原神/gm.zip.002
- 
- wget  https://2264002746.github.io/原神/gm.zip.003
- 
- wget  https://2264002746.github.io/原神/gm.zip.004
- 
- wget  https://2264002746.github.io/原神/gm.zip.005
- 
- wget  https://2264002746.github.io/原神/gm.zip.006
- 
- wget  https://2264002746.github.io/原神/gm.zip.007
- 
- wget  https://2264002746.github.io/原神/gm.zip.008
- 
- wget  https://2264002746.github.io/原神/gm.zip.009
- 
- wget  https://2264002746.github.io/原神/gm.zip.010
- 
- wget  https://2264002746.github.io/原神/gm.zip.011
- 
- wget  https://2264002746.github.io/原神/gm.zip.012
- 
- wget  https://2264002746.github.io/原神/gm.zip.013
- ​echo​ ​"​安装ZIP.....​" 
- ​pkg install zip -y 
- echo "解压文件"
- termux-setup-storage
- cat ./gm.zip.* > gm.zip
- unzip ./gm.zip
- rm -rf ./gm.zip.*
- ​echo​ ​"​安装完成​" 
-  
-  
- ​} 
- ​function​ ​mysql​ { 
- ​clear 
   echo​ ​"​运行jar​" 
   java -jar gm.jar
  ​} 
@@ -234,7 +178,7 @@
  ​echo​ ​"​等待安装.....​" 
   
  ​} 
- ​function​ ​menu​ { 
+ ​function​ ​menu​ {
  ​clear  
  ​echo 
  ​echo​ -e ​"​\t\t\t 应用主菜单​" 
@@ -242,116 +186,5 @@
  ​echo​ -e ​"​\t2.启动服务器端(安卓)​"​  
  ​echo​ -e ​"​\t3. 面具安装​"​  
  ​echo​ -e ​"​\t0. 退出脚本\n\n​" 
- ​echo​ -en ​"​\t\t输入安装序列号:​"​  
- ​read​ -n 1 option 
- ​} 
-  
- ​while​ [ 1 ] 
- ​do​  
- ​    menu 
- ​    ​case​ ​$option​ ​in 
- ​    0)break ;; 
- ​    1)arm ;; 
- ​    2)mysql ;; 
- ​    3)Magisk ;; 
- ​    ​*​) 
- ​        clear 
- ​        ​echo​ ​"​404​"​ ;; 
- ​    ​esac 
- ​    ​echo​ -en ​"​\n\n\t\t 按任意键返回​" 
- ​    ​read​ -n 1 line 
- ​done 
- ​clear
-
- function mysql { 
-
- clear 
-
-  echo "运行jar" 
-
-  java -jar gm.jar
-
- } 
-
- function Magisk { 
-
- clear 
-
- echo "等待安装....." 
-
-  
-
- } 
-
- function menu { 
-
- clear  
-
- echo 
-
- echo -e "\t\t\t 应用主菜单" 
-
- echo -e "\t1.原神服务器资源(安卓)" 
-
- echo -e "\t2.启动服务器端(安卓)"  
-
- echo -e "\t3. 面具安装"  
-
- echo -e "\t0. 退出脚本\n\n" 
-
- echo -en "\t\t输入安装序列号:"  
-
- read -n 1 option 
-
- } 
-
-  
-
- while [ 1 ] 
-
- do  
-
-     menu 
-
-     case $option in 
-
-     0)break ;; 
-
-     1)arm ;; 
-
-     2)mysql ;; 
-
-     3)Magisk ;; 
-
-     *) 
-
-         clear 
-
-         echo "404" ;; 
-
-     esac 
-
-     echo -en "\n\n\t\t 按任意键返回" 
-
-     read -n 1 line 
-
- done 
-
- clear02746.github.io/原神/gm.zip.005
- wget https://2264002746.github.io/原神/gm.zip.006
- wget https://2264002746.github.io/原神/gm.zip.007
- wget https://2264002746.github.io/原神/gm.zip.008
- wget https://2264002746.github.io/原神/gm.zip.009
- wget https://2264002746.github.io/原神/gm.zip.010
- wget https://2264002746.github.io/原神/gm.zip.011
- wget https://2264002746.github.io/原神/gm.zip.012
- wget https://2264002746.github.io/原神/gm.zip.013
- ​echo​ ​"​安装软件.....​" 
- ​pkg install zip -y
- pkg install openjdk-17 -y
- echo "解压文件"
- cat ./gm.zip.* > gm.zip
- unzip ./gm.zip
- rm -rf ./gm.zip.*
- ​echo​ ​"​安装完成​" 
-  java -jar gm.jar
+ ​echo​ -en ​"​\t\t输入安
+    
